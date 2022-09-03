d05f8969bd8d4eb498e6d9db0b784d2c0807b23c
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-09-03 17:37:43 +0200

    00004 - prepare the demo

--- src/App.tsx
+++ src/App.tsx
@@ -24,11 +24,18 @@ const App = () => {
 
 
 
-  const onClick = () => {
+  const onClick = async() => {
     if (!ref.current) return;
 
-    console.log(ref.current);
+    const result = await ref.current.transform(input, {
+      loader: "jsx",
+      target: "es2015"
+    })
+
+    console.log(result);
+    setCode(result.code);
   }
+
 
   return (
     <div>
