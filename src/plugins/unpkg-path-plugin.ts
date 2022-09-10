d671ced2386b7cbbe6aadcaf35fe522a2a0ab793
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-09-10 12:02:29 +0200

    00008 - prepare the demo

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -12,20 +12,37 @@ export const unpkgPathPlugin = () => {
         return { path: args.path, namespace: "a" };
       });
 
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log("onLoad", args);
+        
 
+        
+        if (args.path === "index.js") {
+          return {
+            loader: "jsx",
+            contents: `
+              import message from './message';
+              console.log(message);
+            `,
+          };
+        } else {
+          return {
+            loader: "jsx",
+            contents: 'export default "hi there!"',
+          };
+        }
 
-      
+        
+      });
     },
   };
 };
-
-
 
 /**
  * index.js
  * import message from './message';
  * console.log(message);
- * 
+ *
  * massage.js
  * export default "hi there!"
  */
